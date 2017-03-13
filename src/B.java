799aaf7b89a56e45381a7d0a457bee37b9016732
grekpv <grek.polli@gmail.com>
2017-03-13 10:43:26 +0300

    Initial commit

---
+++ src/B.java
@@ -0,0 +1,19 @@
+/**
+ * Created by grekpv.18 on 13.03.2017.
+ */
+import java.util.Scanner;
+public class B {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int cnt = 0;
+        for (int i = 1; i < a; i++){
+            for (int j = 1; j < a; j++){
+                if (i%j == 0 && i != j)
+                    cnt++;
+            }
+            if (cnt == 0)
+            System.out.println(i);
+        }
+    }
+}
