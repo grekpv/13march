d29ba4c697ac2dcc4476ebbed0247e083ffcca51
grekpv <grek.polli@gmail.com>
2017-03-18 19:56:44 +0300

    commit

---
+++ src/C.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Юлия on 18.03.2017.
+ */
+import java.util.Scanner;
+
+public class C {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cnt = sc.nextInt();
+        double sum = 0;
+        double a;
+        for (int i = 0; i < cnt; i++){
+            a = sc.nextInt();
+            sum = sum+a;
+        }
+        System.out.println(sum/cnt);
+    }
+}
